1f56a82b27a52305e39e5cd42114bea5d6673319
will <maoguangxian@famulei.com>
2018-05-24 12:20:45 +0800

    modify service-python support trace

--- service/python/v1/main.py
+++ service/python/v1/main.py
@@ -1,8 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g, request
 import platform
 import requests
+import logging
 
 app = Flask(__name__)
+
+
+def getForwardHeaders(request):
+    headers = {}
+    incoming_headers = [
+        'x-request-id',
+        'x-b3-traceid',
+        'x-b3-spanid',
+        'x-b3-parentspanid',
+        'x-b3-sampled',
+        'x-b3-flags',
+        'x-ot-span-context'
+    ]
+
+    for ihdr in incoming_headers:
+        val = request.headers.get(ihdr)
+        if val is not None:
+            headers[ihdr] = val
+            # print("incoming: "+ihdr+":"+val)
+
+    return headers
+
+
+@app.before_request
+def before_request():
+    g.forwardHeaders = getForwardHeaders(request)
+
+
+@app.after_request
+def after_request(response):
+    for k, v in g.forwardHeaders.iteritems():
+        response.headers.set(k, v)
+    logging.debug(g.forwardHeaders)
+    return response
 
 
 @app.route("/env")
--- service/python/v2/main.py
+++ service/python/v2/main.py
@@ -1,6 +1,36 @@
-from apistar import App, Route
+from apistar import App, Route, http
 import platform
 import requests
+import logging
+
+
+def getForwardHeaders(request):
+    headers = {}
+    incoming_headers = [
+        'x-request-id',
+        'x-b3-traceid',
+        'x-b3-spanid',
+        'x-b3-parentspanid',
+        'x-b3-sampled',
+        'x-b3-flags',
+        'x-ot-span-context'
+    ]
+
+    for ihdr in incoming_headers:
+        val = request.headers.get(ihdr)
+        if val is not None:
+            headers[ihdr] = val
+            # print("incoming: "+ihdr+":"+val)
+
+    return headers
+
+
+class CustomHeadersHook:
+    def on_response(self, request: http.Request, response: http.Response):
+        forwardHeaders = getForwardHeaders(request)
+        for k, v in forwardHeaders.items():
+            response.headers.set(k, v)
+        logging.debug(forwardHeaders)
 
 
 def env():
@@ -20,8 +50,9 @@ routes = [
     Route('/status', method='GET', handler=status),
 ]
 
-app = App(routes=routes)
+event_hooks = [CustomHeadersHook]
+app = App(routes=routes, event_hooks=event_hooks)
 
 
 if __name__ == '__main__':
-    app.serve('0.0.0.0', 80)
+    app.serve('0.0.0.0', 80, debug=True)
