193ac223cc346a716e8939c30bdaa033c5c9c25d
will <maoguangxian@famulei.com>
2018-05-24 15:38:53 +0800

    modify service go use trace

--- service/go/v2/main.go
+++ service/go/v2/main.go
@@ -6,8 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var incomingHeaders = []string{
+	"x-request-id",
+	"x-b3-traceid",
+	"x-b3-spanid",
+	"x-b3-parentspanid",
+	"x-b3-sampled",
+	"x-b3-flags",
+	"x-ot-span-context",
+}
+
+// TraceMiddleware add trace header
+func TraceMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		for _, h := range incomingHeaders {
+			if ih, ok := c.Request.Header[h]; ok {
+				c.Writer.Header().Set(h, ih[0])
+			}
+		}
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(TraceMiddleware())
 	r.GET("/env", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Go" + runtime.Version(),
